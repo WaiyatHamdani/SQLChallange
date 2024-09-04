4369b344a4b655b42a78455e43faa661a3ce1399
waiyat.hamdani <waiyat.hamdani@fdmgroup.com>
2024-09-04 15:43:56 -0400

    k idol challange postgress 2024

--- Kidol_Challanges.sql
+++ Kidol_Challanges.sql
@@ -22,12 +22,13 @@ ranking_year (INT)
 popularity_rank (INT)
 
  */
-
-drop table if exists Idols;
-drop table if exists Groups;
 drop table if exists Popularity;
+drop table if exists Groups;
+drop table if exists Idols;
 
 
+
+--create table task
 create table Idols(
 idol_id int primary key,
 name varchar(50),
@@ -51,23 +52,53 @@ constraint FK_idol_id foreign key(idol_id) references Idols(idol_id)
 );
 
 -- Idols Table
+
 INSERT INTO Idols (idol_id, name, gender, birth_year, debut_year, group_id) VALUES
-(1, 'Jimin', 'Male', 1995, 2013, 1),
-(2, 'Lisa', 'Female', 1997, 2016, 2),
-(3, 'V', 'Male', 1995, 2013, 1),
-(4, 'Jennie', 'Female', 1996, 2016, 2);
+(1, 'Eunha', 'Female', 1997, 2015, 1),
+(2, 'Sowon', 'Female', 1995, 2015, 1),
+(3, 'SinB', 'Female', 1998, 2015, 1),
+(4, 'Umji', 'Female', 1998, 2015, 1),
+(5, 'Soyeon', 'Female', 1998, 2018, 2),
+(6, 'Miyeon', 'Female', 1997, 2018, 2),
+(7, 'Minnie', 'Female', 1997, 2018, 2),
+(8, 'Yuqi', 'Female', 1999, 2018, 2),
+(9, 'Shuhua', 'Female', 2000, 2018, 2),
+(10, 'Minji', 'Female', 2004, 2022, 3),
+(11, 'Hanni', 'Female', 2004, 2022, 3),
+(12, 'Danielle', 'Female', 2005, 2022, 3),
+(13, 'Haerin', 'Female', 2006, 2022, 3),
+(14, 'Hyein', 'Female', 2008, 2022, 3);
+
+INSERT INTO Idols (idol_id, name, gender, birth_year, debut_year, group_id) VALUES
+(15, 'Jimin', 'Male', 1995, 2013, 4),
+(16, 'V', 'Male', 1995, 2013, 4);
 
 -- Groups Table
 INSERT INTO Groups (group_id, group_name, debut_year) VALUES
-(1, 'BTS', 2013),
-(2, 'BLACKPINK', 2016);
+(1, 'VIVIZ', 2015),
+(2, '(G)I-DLE', 2018),
+(3, 'NewJeans', 2022);
+
+INSERT INTO Groups (group_id, group_name, debut_year) values (4,'BTS',2013);
 
 -- Popularity Table
 INSERT INTO Popularity (idol_id, ranking_year, popularity_rank) VALUES
-(1, 2023, 1),
-(2, 2023, 3),
-(3, 2023, 2),
-(4, 2023, 4);
+(1, 2023, 5),
+(2, 2023, 7),
+(3, 2023, 6),
+(4, 2023, 8),
+(5, 2023, 1),
+(6, 2023, 3),
+(7, 2023, 4),
+(8, 2023, 2),
+(9, 2023, 9),
+(10, 2023, 1),
+(11, 2023, 2),
+(12, 2023, 3),
+(13, 2023, 4),
+(14, 2023, 5);
+
+INSERT INTO Popularity (idol_id, ranking_year, popularity_rank) values (15, 2023, 5),(16, 2023, 8);
 
 
 --Task 1: Write a query to retrieve the most popular male idols , sorted by their popularity rank.
@@ -76,11 +107,14 @@ on i.idol_id = p.idol_id
 where gender = 'Male'
 order by popularity_rank asc;
 
---Task 2: Write a query to find all idols who debuted before the age of 19, along with their group name and debut year.
-select * from Idols where debut_year - birth_year < 19; 
+--Task 2: Write a query to find all idols who debuted before the age of 18, along with their group name and debut year.
+select * from Idols where debut_year - birth_year < 18; 
 
---Task 3: Write a query to find the  popularity rank for each group in the 2023. Display the group name along with their average rank.
+--Task 3: Write a query to find the  popularity rank 2 for each group in the 2023. Display the group name along with their average rank.
 select * from Idols i join Popularity p
 on i.idol_id  = p.idol_id 
-where ranking_year = '2023'
+where ranking_year = '2023' and popularity_rank = '2'
 order by popularity_rank asc
+
+
+select* from idols 
